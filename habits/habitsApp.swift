203eeee1559469aeece47e1a6653ef149161c78d
Christian Ost <hallo@christianost.de>
2022-02-17 22:27:37 +0100

    Initial Commit

---
+++ habits/habitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  habitsApp.swift
+//  habits
+//
+//  Created by Christian Ost on 17.02.22.
+//
+
+import SwiftUI
+
+@main
+struct habitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
