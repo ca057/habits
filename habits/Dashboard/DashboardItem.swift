bc64469ecf74bf1126acf7469d9ee3f11a436f6f
Christian Ost <hallo@christianost.de>
2022-03-28 21:20:08 +0200

    feat: add first draft for single item, remove delete action from list

--- habits/Dashboard/DashboardItem.swift
+++ habits/Dashboard/DashboardItem.swift
@@ -7,19 +7,58 @@
 
 import SwiftUI
 
+struct DayElement: View {
+    let day: Date
+    let formattedDay: String
+    let isInWeekend: Bool
+    
+    var body: some View {
+        Button(action: {
+            print(day.description)
+        }, label: {
+            Text(formattedDay)
+                .padding(.horizontal, 8)
+                .padding(.vertical)
+                .background(isInWeekend ? .indigo : .orange)
+                .clipShape(Capsule())
+        })
+    }
+    
+    init(day: Date) {
+        self.day = day
+        self.formattedDay = "\(Calendar.current.dateComponents([.day], from: day).day ?? 0)"
+        self.isInWeekend = Calendar.current.isDateInWeekend(day)
+    }
+}
+
+private extension Date {
+    func getDayBefore(by value: Int) -> Date {
+        return Calendar.current.date(byAdding: .day, value: value, to: self)!
+    }
+}
+
 struct DashboardItem: View {
     var habit: Habit
+    
+    let now = Date.now
+    let today = Calendar.current.dateComponents([.day], from: Date.now)
     
     var body: some View {
         VStack(alignment: .leading, spacing: 4) {
             Text(habit.name ?? "N/A")
                 .font(.title)
                 .bold()
-                .padding(.top)
             Text("Started on \(habit.createdAt?.formatted(date: .abbreviated, time: .omitted) ?? "N/A")")
                 .font(.caption)
                 .padding(.bottom)
+            HStack {
+                ForEach(0..<7, id: \.self) { index in
+                    DayElement(day: now.getDayBefore(by: index - 6))
+                        .frame(maxWidth: .infinity)
+                }
+            }
         }
+        .padding(.vertical)
     }
 }
 
--- habits/Dashboard/DashboardView.swift
+++ habits/Dashboard/DashboardView.swift
@@ -16,10 +16,7 @@ struct DashboardView: View {
             SortDescriptor(\.name),
         ]
     ) var habits: FetchedResults<Habit>
-    
-    @State private var confirmDeletion = false
-    @State private var habitIndexSetToDelete: IndexSet? = nil
-    
+
     var body: some View {
         VStack {
             List {
@@ -29,51 +26,9 @@ struct DashboardView: View {
                     ForEach(habits) { habit in
                         DashboardItem(habit: habit)
                     }
-                    .onDelete { indexSet in
-                        habitIndexSetToDelete = indexSet
-                        confirmDeletion = true
-                    }
                 }
             }
         }
-        .alert("Delete item?", isPresented: $confirmDeletion) {
-            Button("Cancel", role: .cancel) {}
-            Button("Delete", role: .destructive) {
-                if let indexSet = habitIndexSetToDelete {
-                    removeItems(at: indexSet)
-                    habitIndexSetToDelete = nil
-                }
-                confirmDeletion = false
-            }
-        } message: {
-            Text("Do you really want to delete \(getHabitNames(for: habitIndexSetToDelete))?")
-        }
-    }
-    
-    private func removeItems(at offsets: IndexSet) {
-        for offset in offsets {
-            moc.delete(habits[offset])
-        }
-        if moc.hasChanges {
-            try? moc.save()
-        }
-    }
-    
-    private func getHabitNames(for offsets: IndexSet?) -> String {
-        guard let offsets = offsets else {
-            return ""
-        }
-        guard !habits.isEmpty else {
-            return ""
-        }
-
-        var habitList = ""
-        for offset in offsets {
-            let nextHabit = habits[offset].name ?? "N/A"
-            habitList = habitList.count > 0 ? "\(habitList), \(nextHabit)" : nextHabit
-        }
-
-        return habitList
     }
 }
 
