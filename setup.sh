14f24f271e1aba60d3afe107bc87a6e28c3e6e6a
Christian Ost <hallo@christianost.de>
2022-04-15 17:15:00 +0200

    chore: improve dev-setup script

--- setup.sh
+++ setup.sh
@@ -8,15 +8,40 @@ function command_exists() {
 	fi
 }
 
+function install_with_brew() {
+	if ! [ -x "$(command -v $1)" ]; then
+		install_from=$1
+		if [ -n "$2" ]; then
+			install_from=$2
+		fi
+		echo "$1 will be install with 'brew install $install_from'"
+		brew install "$install_from"
+	else
+		echo "$1 already installed, skipping"
+	fi
+}
+
+function install_with_gem() {
+	if ! [ -x "$(command -v $1)" ]; then
+		install_from=$1
+		if [ -n "$2" ]; then
+			install_from=$2
+		fi
+		echo "$1 will be installed with 'gem install $install_from'"
+		gem install "$install_from"
+	else
+		echo "$1 already installed, skipping"
+	fi
+}
+
 echo "setting up dev environment for Habits"
 echo "---"
-command_exists brew
-command_exists gem
+# command_exists brew
+# command_exists gem
 
-gem install xcpretty
-
-# setup pre-commits
-brew install go-task/tap/go-task
+install_with_brew "pre-commit"
+install_with_gem "xcpretty" 
+install_with_brew "task" "go-task/tap/go-task"
 
 echo "---"
 echo "finished setting up dev environment"
