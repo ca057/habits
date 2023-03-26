5e60722b50c163be4d7c5d815272fcabb18b4175
Christian Ost <hallo@christianost.de>
2023-03-26 23:20:58 +0200

    Revert "feat: nicer overview design"
    This reverts commit 36c59b971d668fd3839b160528ba81978d64b8e8.

--- Habits/Features/HabitView/HabitView.swift
+++ Habits/Features/HabitView/HabitView.swift
@@ -7,22 +7,6 @@
 
 import SwiftUI
 
-fileprivate struct SectionHeader: View {
-    private var text: String
-
-    var body: some View {
-        Text(text)
-            .font(.headline)
-            .fontDesign(.rounded)
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(.bottom, 4)
-    }
-    
-    init(_ text: String) {
-        self.text = text
-    }
-}
-
 struct HabitView: View {
     @StateObject var viewModel: ViewModel
     
@@ -32,46 +16,33 @@ struct HabitView: View {
     var body: some View {
         VStack {
             Form {                
-                Section {
-                    VStack {
-                        SectionHeader("Overview")
-
-                        ReverseCalendarView(endDate: viewModel.latestEntry) { date in
-                            if viewModel.hasEntryForDate(date) {
-                                Image(systemName: "checkmark.circle.fill")
-                                    .foregroundColor(viewModel.colour.toColor())
-                            } else if let date = date {
-                                Text(CalendarHelper().calendar.component(.day, from: date).description)
-                                    .font(.footnote.monospacedDigit())
-                                    .fontWeight(date.compare(.isWeekend) ? .light : .regular)
-                            }
+                Section("Overview") {
+                    ReverseCalendarView(endDate: viewModel.latestEntry) { date in
+                        if viewModel.hasEntryForDate(date) {
+                            Image(systemName: "checkmark.circle.fill")
+                                .foregroundColor(viewModel.colour.toColor())
+                        } else if let date = date {
+                            Text(CalendarHelper().calendar.component(.day, from: date).description)
+                                .font(.footnote.monospacedDigit())
+                                .fontWeight(date.compare(.isWeekend) ? .light : .regular)
                         }
-                        // TODO: add load more button here
                     }
+                    // TODO: add load more button here
                 }
                 .buttonStyle(BorderlessButtonStyle())
                 
-                Section {
-                    VStack {
-                        SectionHeader("Settings")
-                        
-                        TextField("Name", text: $viewModel.name)
-                        
-                        VStack(alignment: .leading) {
-                            Text("Colour")
-                            ColourPicker(colours: Colour.allCasesSorted, selection: $viewModel.colour)
-                                .padding(.bottom, 4)
-                        }
+                Section("Settings") {
+                    TextField("Name", text: $viewModel.name)
+                    VStack(alignment: .leading) {
+                        Text("Colour")
+                        ColourPicker(colours: Colour.allCasesSorted, selection: $viewModel.colour)
+                            .padding(.bottom, 4)
                     }
                 }
                 
-                Section {
-                    VStack {
-                        SectionHeader("Danger Zone")
-                        
-                        Button("Delete habit", role: .destructive) {
-                            showDeleteConfirmation = true
-                        }
+                Section("Danger Zone") {
+                    Button("Delete habit", role: .destructive) {
+                        showDeleteConfirmation = true
                     }
                 }
             }
