56d10895d6e6d17dd61129a7afafb72d2d68b7f2
Christian Ost <christian@forget.finance>
2023-08-06 15:04:32 +0200

    feat: explore improved fetch requests

--- Habits/Features/Dashboard/Dashboard-ViewModel.swift
+++ Habits/Features/Dashboard/Dashboard-ViewModel.swift
@@ -7,6 +7,14 @@
 
 import Combine
 import Foundation
+import CoreData
+
+struct DashboardHabit {
+    let id: UUID
+    let name: String
+    let colour: Colour
+    let entries: [Entry]
+}
 
 extension Dashboard {
     @MainActor final class ViewModel: ObservableObject {
@@ -15,11 +23,44 @@ extension Dashboard {
         
         @Published var habits = [Habit]()
         @Published var showingAddHabit = false
+
+        @Published var dashboardHabits = [DashboardHabit]()
         
         init() {
             habitsStorage.$habits
                 .assign(to: \.habits, on: self)
                 .store(in: &cancellables)
+            
+            let habitRequest: NSFetchRequest<Habit> = Habit.fetchRequest()
+            
+            habitRequest.propertiesToFetch = ["name", "colour"]
+            habitRequest.sortDescriptors = [
+                NSSortDescriptor(key: "order", ascending: true),
+                NSSortDescriptor(key: "createdAt", ascending: false)
+            ]
+            
+            let fetchedHabits = try? DataController.shared.container.viewContext.fetch(habitRequest)
+            
+            guard let habitsWithoutEntries = fetchedHabits else { return }
+            
+            habitsWithoutEntries.forEach { habit in
+                guard let id = habit.id, let name = habit.name else { return }
+                        
+                let entryRequest: NSFetchRequest<Entry> = Entry.fetchRequest()
+                entryRequest.predicate = NSPredicate(format: "habit == %@", habit)
+                entryRequest.fetchLimit = 7 // TODO: make it a global setting?
+                let entries = try? DataController.shared.container.viewContext.fetch(entryRequest)
+                
+                
+                dashboardHabits.append(
+                    DashboardHabit(
+                        id: id,
+                        name: name,
+                        colour: Colour.fromRawValue(habit.colour),
+                        entries: entries ?? []
+                    )
+                )
+            }
         }
         
         func toggleEntry(for habit: Habit, on date: Date) {
