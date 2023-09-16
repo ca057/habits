d2c09f11888c13e11737656f6a67b6c874a6f0b6
Christian Ost <christian@forget.finance>
2023-09-16 17:55:32 +0200

    feat: add first header to history view

--- Habits/Components/HistoryView.swift
+++ Habits/Components/HistoryView.swift
@@ -10,25 +10,37 @@ import SwiftUI
 struct HistoryView: View {
     var body: some View {
         ExpandableRows(
-            minimumRows: 2,
+            minimumRows: 4,
             header: { Header() }
         ) { rowIndex in
             Week(calendarWeek: rowIndex)
-        }
+        }.background(.blue.opacity(0.1))
     }
 }
 
 extension HistoryView {
     private struct Header: View {
         var body: some View {
-            Text("Header")
+            HStack(spacing: 0) {
+                Spacer()
+                    .frame(maxWidth: .infinity)
+                ForEach(0..<CalendarUtils.shared.weekDays.count, id: \.self) { index in
+                    Text(CalendarUtils.shared.weekDays[index])
+                        .font(.subheadline)
+                        .fontDesign(.monospaced)
+                        // TODO: find a better style here
+                        .fontWeight(index < 5 ? .regular : .bold)
+                        .frame(maxWidth: .infinity)
+                        .border(.black)
+                }
+            }
         }
     }
     
     private struct Week: View {
         var calendarWeek: Int
         var body: some View {
-            Text("Week \(calendarWeek)")
+            Text("Week: \(calendarWeek)")
         }
     }
 }
