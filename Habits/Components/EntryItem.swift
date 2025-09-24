965adb2cc21ca3367af0249dbaf17ea767897463
Christian Ost <hallo@christianost.de>
2025-09-24 22:25:43 +0200

    feat: add specific button variant

--- Habits/Components/EntryItem.swift
+++ Habits/Components/EntryItem.swift
@@ -7,6 +7,30 @@
 
 import SwiftUI
 
+struct EntryItemButton: View {
+    var count: Int = 0
+    var color = Color.primary
+    var secondaryColor = Color.secondary
+    var size: CGFloat = 12
+    var highlighted = false
+    var onTap: () -> Void = {}
+    
+    var body: some View {
+        Button(action: onTap) {
+            EntryItem(count: count, color: color)
+        }.buttonStyle(.borderless)
+    }
+    
+    init(count: Int = 0, color: SwiftUICore.Color = Color.primary, secondaryColor: Color = Color.secondary, highlighted: Bool = false, size: CGFloat = CGFloat(24), perform onTap: @escaping () -> Void) {
+        self.count = count
+        self.color = color
+        self.secondaryColor = secondaryColor
+        self.highlighted = highlighted
+        self.size = size
+        self.onTap = onTap
+    }
+}
+
 struct EntryItem: View {
     var count: Int = 0
     var color = Color.primary
@@ -19,7 +43,6 @@ struct EntryItem: View {
     }
     
     var body: some View {
-//            RoundedRectangle(cornerSize: CGSize(width: 2, height: 2))
         Circle()
             .frame(width: actualSize, height: actualSize)
             .foregroundStyle(count > 0 ? color : secondaryColor)
@@ -51,4 +74,13 @@ struct EntryItem: View {
     EntryItem(highlighted: true)
     EntryItem(count: 1, highlighted: true)
     EntryItem(count: 1, color: .blue, highlighted: true)
+    
+    EntryItemButton(perform: { print("tap") })
+    EntryItemButton(count: 1, perform: { print("tap") })
+    EntryItemButton(count: 0, color: .green, secondaryColor: .green.mix(with: .white, by: 0.5), perform: { print("tap") })
+    EntryItemButton(count: 1, color: .green, secondaryColor: .green.opacity(0.5), perform: { print("tap") })
+    EntryItemButton(highlighted: true, perform: { print("tap") })
+    EntryItemButton(count: 1, highlighted: true, perform: { print("tap") })
+    EntryItemButton(count: 1, color: .blue, highlighted: true, perform: { print("tap") })
+
 }
