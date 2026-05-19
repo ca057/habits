7bba8724dd0401110409c9859aa49729257e0c5a
Christian Ost <hallo@christianost.de>
2026-05-19 18:54:22 +0200

    test: cases for import

--- HabitsTests/Features/Settings/DataImportExportServiceTests.swift
+++ HabitsTests/Features/Settings/DataImportExportServiceTests.swift
@@ -22,7 +22,8 @@ struct DataImportExportServiceTests {
             DataImportExportService(appVersion: "1.2.3", calendar: calendar)
         }
 
-        @Test("imports backup file with entries containing a date property") func importsBackupFileWithEntriesWithDate() throws {
+        @Test("imports backup file with entries containing a date property")
+        func importsBackupFileWithEntriesWithDate() throws {
             let backupJson = """
                 {
                     "appVersion": "1.2.3",
@@ -53,12 +54,63 @@ struct DataImportExportServiceTests {
             #expect(items.first?.entries[1].day == "2026-03-22")
         }
 
-        @Test("imports backup file with entries containing a day property") func importsBackupFileWithEntriesWithDay() throws {
-            // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        @Test("imports backup file with entries containing a day property")
+        func importsBackupFileWithEntriesWithDay() throws {
+            let backupJson = """
+                {
+                    "appVersion": "1.2.3",
+                    "exportDate": "2026-04-21T23:00:00Z",
+                    "habits": [
+                        {
+                            "id": "E1A185CD-06BA-4C4B-9B2C-E4C33AE42F3B",
+                            "name": "Test",
+                            "createdAt": "2026-02-25T23:00:00Z",
+                            "colour": "green",
+                            "order": 0,
+                            "entries": [
+                                { "day": "2026-02-26" },
+                                { "day": "2026-03-22" }
+                            ],
+                        }
+                    ]
+                }
+                """
+
+            let items = try service.importHabitsFromJsonFile(from: Data(backupJson.utf8))
+            
+            #expect(items.count == 1)
+            
+            #expect(items.first?.entries.count == 2)
+            #expect(items.first?.id.uuidString == "E1A185CD-06BA-4C4B-9B2C-E4C33AE42F3B")
+            #expect(items.first?.entries[0].day == "2026-02-26")
+            #expect(items.first?.entries[1].day == "2026-03-22")
         }
 
-        @Test("fails gracefully when importing a backup file where entries have neither a day nor date property") func failsForImportOfBackupFileWithEntriesWithNeitherDateOrDay() throws {
-            // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        @Test("fails gracefully when importing a backup file where entries have neither a day nor date property")
+        func failsForImportOfBackupFileWithEntriesWithNeitherDateOrDay() throws {
+            let backupJson = """
+                {
+                    "appVersion": "1.2.3",
+                    "exportDate": "2026-04-21T23:00:00Z",
+                    "habits": [
+                        {
+                            "id": "E1A185CD-06BA-4C4B-9B2C-E4C33AE42F3B",
+                            "name": "Test",
+                            "createdAt": "2026-02-25T23:00:00Z",
+                            "colour": "green",
+                            "order": 0,
+                            "entries": [
+                                {},
+                            ],
+                        }
+                    ]
+                }
+                """
+
+            let items = try service.importHabitsFromJsonFile(from: Data(backupJson.utf8))
+            
+            #expect(items.count == 1)
+            #expect(items.first?.entries.count == 0)
         }
     }
     
